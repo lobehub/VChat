b6d9840a6920c66e6a612edf3a2b1c672138b5db
rdmclin2 <rdmclin2@gmail.com>
2023-06-24 13:11:14 +0800

    feat: render with VrmViewer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  assetPrefix: process.env.BASE_PATH || "",
+  basePath: process.env.BASE_PATH || "",
+  trailingSlash: true,
+  publicRuntimeConfig: {
+    root: process.env.BASE_PATH || "",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
