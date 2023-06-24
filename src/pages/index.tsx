8c0893c7b8c36260007c195189be704fac9e16ad
rdmclin2 <rdmclin2@gmail.com>
2023-06-24 23:00:34 +0800

    feat: add footer and header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import styles from "./page.module.css";
-import VrmViewer from "@/components/vrmViewer";
+import { Layout } from '@lobehub/ui';
+import VrmViewer from '@/components/vrmViewer';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <VrmViewer />
-    </main>
+    <Layout footer={<Footer />} header={<Header />} sidebar={<VrmViewer />}>
+      123
+    </Layout>
   );
 }
