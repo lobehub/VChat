233bf5350d605f6cd0d7027996d013248a4a3a48
rdmclin2 <rdmclin2@gmail.com>
2023-06-24 23:47:38 +0800

    feat: add theme

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,33 @@
-import { Header as LobeHeader,TabsNav  } from '@lobehub/ui';
+import { Header as LobeHeader, TabsNav, ThemeSwitch } from '@lobehub/ui';
+import { type ThemeMode } from 'antd-style';
+import { useThemeStore } from '@/store/useThemeStore';
 
-const Header =  () => {
-  return <LobeHeader actions={'ACTIONS'} logo={'LOGO'} nav={<TabsNav
-    items={[
-      {
-        key: 'home',
-        label: 'Home',
-      },
-      {
-        key: 'components',
-        label: 'Components',
-      },
-      {
-        key: 'changelog',
-        label: 'Changelog',
-      },
-    ]}
-  />} />;
+const Header = () => {
+  const [themeMode, setThemeMode] = useThemeStore((s) => [s.themeMode, s.setThemeMode]);
+  return (
+    <LobeHeader
+      actions={[<ThemeSwitch onThemeSwitch={setThemeMode} themeMode={themeMode} key="theme" />]}
+      logo={'Vidol.Chat'}
+      // nav={
+      //   <TabsNav
+      //     items={[
+      //       {
+      //         key: 'home',
+      //         label: '首页',
+      //       },
+      //       {
+      //         key: 'docs',
+      //         label: '使用文档',
+      //       },
+      //       {
+      //         key: 'donate',
+      //         label: 'Buy me Coffee',
+      //       },
+      //     ]}
+      //   />
+      // }
+    />
+  );
 };
 
 export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
-import { Layout } from '@lobehub/ui';
-import VrmViewer from '@/components/vrmViewer';
+import { Layout, ThemeProvider } from '@lobehub/ui';
+import VrmViewer from '@/components/VrmViewer';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { useThemeStore } from '@/store/useThemeStore';
 
 export default function Home() {
+  const themeMode = useThemeStore((s) => s.themeMode);
   return (
-    <Layout footer={<Footer />} header={<Header />} sidebar={<VrmViewer />}>
-      123
-    </Layout>
+    <ThemeProvider themeMode={themeMode}>
+      <Header />
+      <main>
+        <VrmViewer />
+      </main>
+      <Footer />
+    </ThemeProvider>
   );
 }
