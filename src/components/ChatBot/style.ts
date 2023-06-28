d52ef3efde4ea6dfeb42c580bc0fe9fcd15724e3
rdmclin2 <rdmclin2@gmail.com>
2023-06-28 23:34:08 +0800

    fix: expand backgound transparent and add mic action

--- src/components/ChatBot/style.ts
+++ src/components/ChatBot/style.ts
@@ -2,6 +2,7 @@ import { createStyles } from 'antd-style';
 
 const useStyles = createStyles(({ token, css }) => ({
   chatbot: css`
+    position: relative;
     display: flex;
     flex-direction: column;
     height: calc(100vh - 64px);
