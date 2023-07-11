8e3309f9c17800a7669c3d7267216e52afc0f771
rdmclin2 <rdmclin2@gmail.com>
2023-07-11 23:20:48 +0800

    feat: add stream api

--- src/lib/Agent/index.ts
+++ src/lib/Agent/index.ts
@@ -1,6 +1,6 @@
 import { OpenAI } from 'langchain/llms/openai';
 
-const OPENAI_API_KEY = '';
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const model = new OpenAI({ openAIApiKey: OPENAI_API_KEY, temperature: 0.9 });
 
