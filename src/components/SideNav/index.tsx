e354439fa3b6864c1f8a50c583733f86e8259708
rdmclin2 <rdmclin2@gmail.com>
2023-07-20 23:35:08 +0800

    chore: 添加 config 配置页面

--- src/components/SideNav/index.tsx
+++ src/components/SideNav/index.tsx
@@ -1,26 +1,35 @@
 import { ActionIcon, SideNav as LobeSideNav } from '@lobehub/ui';
-import { Album, MessageSquare, Settings2 } from 'lucide-react';
+import { MessageSquare, Settings2 } from 'lucide-react';
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 const SideNav = () => {
   const [tab, setTab] = useState<string>('chat');
+  const router = useRouter();
 
   return (
     <LobeSideNav
       style={{ height: 'calc(100vh - 64px)' }}
-      bottomActions={<ActionIcon icon={Settings2} />}
+      bottomActions={
+        <ActionIcon
+          icon={Settings2}
+          onClick={() => {
+            setTab('config');
+            router.push('/config');
+          }}
+          size="large"
+          active={tab === 'config'}
+        />
+      }
       topActions={
         <>
           <ActionIcon
             active={tab === 'chat'}
             icon={MessageSquare}
-            onClick={() => setTab('chat')}
-            size="large"
-          />
-          <ActionIcon
-            active={tab === 'market'}
-            icon={Album}
-            onClick={() => setTab('market')}
+            onClick={() => {
+              setTab('chat');
+              router.push('/');
+            }}
             size="large"
           />
         </>
--- src/pages/config/index.tsx
+++ src/pages/config/index.tsx
@@ -12,80 +12,82 @@ const setting = {
 
 const Config = () => {
   return (
-    <Form initialValues={setting} onFinish={console.table} style={{ width: 500 }}>
-      <FormGroup icon={Palette} title={'Theme Settings'}>
-        <FormItem desc={'Editor language'} label={'Language'} name="i18n">
-          <Select
-            options={[
-              {
-                label: 'English',
-                value: 'en',
-              },
-              {
-                label: '简体中文',
-                value: 'zh_CN',
-              },
-            ]}
-          />
-        </FormItem>
-        <FormItem
-          desc={
-            'Reduce the blur effect and background flow color, which can improve smoothness and save CPU usage'
-          }
-          divider
-          label={'Reduce Animation'}
-          name="liteAnimation"
-          valuePropName="checked"
-        >
-          <Switch />
-        </FormItem>
-      </FormGroup>
-      <FormGroup icon={PanelLeftClose} title={'Quick Setting Sidebar'}>
-        <FormItem
-          desc={'Whether to expand the sidebar by default when starting'}
-          label={'Default Expand'}
-          name="sidebarExpand"
-          valuePropName="checked"
-        >
-          <Switch />
-        </FormItem>
-        <FormItem
-          desc={
-            'Fixed as grid mode for constant display, auto-expand when the mouse moves to the side in floating mode'
-          }
-          divider
-          label={'Display Mode'}
-          name="sidebarFixedMode"
-        >
-          <Segmented
-            options={[
-              {
-                label: 'Fixed',
-                value: 'fixed',
-              },
-              {
-                label: 'Float',
-                value: 'float',
-              },
-            ]}
-          />
-        </FormItem>
-        <FormItem
-          desc={'Default width of the sidebar when starting'}
-          divider
-          label={'Default Width'}
-          name="sidebarWidth"
-        >
-          <InputNumber />
-        </FormItem>
-      </FormGroup>
-      <FormFooter>
-        <Button htmlType="button">Reset</Button>
-        <Button htmlType="submit" type="primary">
-          Submit
-        </Button>
-      </FormFooter>
-    </Form>
+    <div style={{ padding: 24 }}>
+      <Form initialValues={setting} onFinish={console.table}>
+        <FormGroup icon={Palette} title={'Theme Settings'}>
+          <FormItem desc={'Editor language'} label={'Language'} name="i18n">
+            <Select
+              options={[
+                {
+                  label: 'English',
+                  value: 'en',
+                },
+                {
+                  label: '简体中文',
+                  value: 'zh_CN',
+                },
+              ]}
+            />
+          </FormItem>
+          <FormItem
+            desc={
+              'Reduce the blur effect and background flow color, which can improve smoothness and save CPU usage'
+            }
+            divider
+            label={'Reduce Animation'}
+            name="liteAnimation"
+            valuePropName="checked"
+          >
+            <Switch />
+          </FormItem>
+        </FormGroup>
+        <FormGroup icon={PanelLeftClose} title={'Quick Setting Sidebar'}>
+          <FormItem
+            desc={'Whether to expand the sidebar by default when starting'}
+            label={'Default Expand'}
+            name="sidebarExpand"
+            valuePropName="checked"
+          >
+            <Switch />
+          </FormItem>
+          <FormItem
+            desc={
+              'Fixed as grid mode for constant display, auto-expand when the mouse moves to the side in floating mode'
+            }
+            divider
+            label={'Display Mode'}
+            name="sidebarFixedMode"
+          >
+            <Segmented
+              options={[
+                {
+                  label: 'Fixed',
+                  value: 'fixed',
+                },
+                {
+                  label: 'Float',
+                  value: 'float',
+                },
+              ]}
+            />
+          </FormItem>
+          <FormItem
+            desc={'Default width of the sidebar when starting'}
+            divider
+            label={'Default Width'}
+            name="sidebarWidth"
+          >
+            <InputNumber />
+          </FormItem>
+        </FormGroup>
+        <FormFooter>
+          <Button htmlType="button">Reset</Button>
+          <Button htmlType="submit" type="primary">
+            Submit
+          </Button>
+        </FormFooter>
+      </Form>
+    </div>
   );
 };
 
