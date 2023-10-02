58c5be94747c49dac7cd86395fec3a764178048d
rdmclin2 <rdmclin2@gmial.com>
2023-10-02 20:53:50 +0800

    chore: add git download

--- src/pages/api/download.ts
+++ src/pages/api/download.ts
@@ -1,3 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { simpleGit } from 'simple-git';
+
 export default function handler(req, res) {
-  res.status(200).json({});
+  const { url } = req.body;
+  const git = simpleGit();
+
+  const agentsDir = path.join(process.cwd(), '/public/agents');
+  if (!fs.existsSync(agentsDir)) {
+    fs.mkdirSync(agentsDir);
+  }
+  try {
+    git.clone(url, agentsDir);
+    res.status(200).json({});
+  } catch (err) {
+    console.log(err);
+    res.status(200).json({});
+  }
 }
