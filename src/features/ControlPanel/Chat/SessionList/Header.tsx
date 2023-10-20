6a6a89369a10ee71879d63c6bd09ba892dfe2d1f
rdmclin2 <rdmclin2@gmail.com>
2023-10-20 23:25:24 +0800

    feat: add button

--- src/features/ControlPanel/Chat/SessionList/Header.tsx
+++ src/features/ControlPanel/Chat/SessionList/Header.tsx
@@ -1,4 +1,6 @@
-import { SearchBar } from '@lobehub/ui';
+import { useConfigStore } from '@/store/config';
+import { ActionIcon, SearchBar } from '@lobehub/ui';
+import { Plus } from 'lucide-react';
 import { memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
@@ -6,11 +8,14 @@ import { useStyles } from './style';
 
 // eslint-disable-next-line react/display-name
 const Header = memo(() => {
-  const { styles, theme } = useStyles();
+  const { styles } = useStyles();
+  const { setTab } = useConfigStore();
 
   return (
-    <Flexbox className={styles.container} gap={16}>
-      <SearchBar shortKey="f" enableShortKey />
+    <Flexbox className={styles.container} direction="horizontal" gap={16}>
+      <SearchBar shortKey="f" enableShortKey placeholder="搜索" />
+      {/* @ts-ignore */}
+      <ActionIcon icon={Plus} onClick={() => setTab('agent')} />
     </Flexbox>
   );
 });
