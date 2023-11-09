c13dc4b1de18fe229047d2b1dabd06dbd35e2e00
rdmclin2 <rdmclin2@gmail.com>
2023-11-09 23:33:03 +0800

    chore: add default touch actions

--- src/store/touch.ts
+++ src/store/touch.ts
@@ -39,10 +39,74 @@ const DEFAULT_TOUCH_ACTION_CONFIG: TouchActionConfig = {
       enabled: true,
     },
   ],
-  [TouchAreaEnum.Arm]: [],
-  [TouchAreaEnum.Leg]: [],
-  [TouchAreaEnum.Chest]: [],
-  [TouchAreaEnum.Belly]: [],
+  [TouchAreaEnum.Arm]: [
+    {
+      text: '啊，好喜欢呢~',
+      enabled: true,
+    },
+    {
+      text: '主人的手好温暖啊~',
+      enabled: true,
+    },
+    {
+      text: '哈哈，牵手让我感到快乐~',
+      enabled: true,
+    },
+  ],
+  [TouchAreaEnum.Leg]: [
+    {
+      text: '让我们保持纯洁的友谊不好吗？',
+      enabled: true,
+    },
+    {
+      text: '喂，你是要作死吗?',
+      enabled: true,
+    },
+    {
+      text: '主人的手又不听指挥了吗?',
+      enabled: true,
+    },
+    {
+      text: '讨厌~会痒的啦~!',
+      enabled: true,
+    },
+  ],
+  [TouchAreaEnum.Chest]: [
+    {
+      text: '不可以这样欺负我啦！快把手拿开！',
+      enabled: true,
+    },
+    {
+      text: '幺幺零吗？这里有个变态一直在摸我！',
+      enabled: true,
+    },
+    {
+      text: '再摸的话我可要报警了',
+      enabled: true,
+    },
+    {
+      text: '干嘛戳我呀！还能不能愉快地聊天了!',
+      enabled: true,
+    },
+  ],
+  [TouchAreaEnum.Belly]: [
+    {
+      text: '是不小心碰到的吧...',
+      enabled: true,
+    },
+    {
+      text: '干嘛动我呀，小心我咬你哦！',
+      enabled: true,
+    },
+    {
+      text: '醒醒，我们之间没有结果的!',
+      enabled: true,
+    },
+    {
+      text: '讨厌！我可要生气啦！',
+      enabled: true,
+    },
+  ],
   enabled: true,
 };
 
