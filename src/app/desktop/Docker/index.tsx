a8b21b9349ba71037556184b819b5b4912cbd8de
rdmclin2 <rdmclin2@gmail.com>
2024-02-03 23:13:51 +0800

    feat: add Docker App

---
+++ src/app/desktop/Docker/index.tsx
@@ -0,0 +1,88 @@
+'use client';
+
+import { useConfigStore } from '@/store/config';
+import { PanelKey } from '@/types/config';
+import { ActionIconGroup } from '@lobehub/ui';
+import { MessageSquare, Music2, Settings2, ShoppingBag, User, Video } from 'lucide-react';
+
+import {
+  AgentPanel,
+  ChatPanel,
+  ConfigPanel,
+  DancePanel,
+  LivePanel,
+  MarketPanel,
+  RolePanel,
+} from '@/panels';
+
+const apps = [
+  {
+    icon: User,
+    key: 'agent',
+    label: '联系人',
+    component: <AgentPanel />,
+  },
+  {
+    icon: Music2,
+    key: 'dance',
+    label: '跳舞',
+    component: <DancePanel />,
+  },
+  {
+    icon: MessageSquare,
+    key: 'chat',
+    label: '聊天',
+    component: <ChatPanel />,
+  },
+  {
+    icon: Video,
+    key: 'live',
+    label: '视频',
+    component: <LivePanel />,
+  },
+  {
+    icon: ShoppingBag,
+    key: 'market',
+    label: '商店',
+    component: <MarketPanel />,
+  },
+  {
+    icon: Settings2,
+    key: 'config',
+    label: '设置',
+    component: <ConfigPanel />,
+  },
+  {
+    icon: User,
+    key: 'role',
+    label: '角色',
+    component: <RolePanel />,
+  },
+];
+
+const Docker = () => {
+  const [panel, setPanel] = useConfigStore((s) => [s.panel, s.setPanel]);
+
+  return (
+    <div style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+      <ActionIconGroup
+        items={apps}
+        style={{ height: 48, alignItems: 'center', justifyContent: 'center' }}
+        onActionClick={(action) => {
+          setPanel(action.key as any, { open: true });
+        }}
+      />
+      {apps.map((app) => {
+        const open = panel[app.key as PanelKey].open;
+        const component = app.component;
+        return (
+          <div key={app.key} style={{ display: open ? 'flex' : 'none' }}>
+            {component}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Docker;
--- src/app/desktop/page.tsx
+++ src/app/desktop/page.tsx
@@ -1,90 +1,14 @@
 'use client';
 
 import Background from '@/app/desktop/Background';
+import Docker from '@/app/desktop/Docker';
 import Header from '@/app/desktop/Header';
-import { useConfigStore } from '@/store/config';
-import { PanelKey } from '@/types/config';
-import { ActionIconGroup } from '@lobehub/ui';
-import { MessageSquare, Music2, Settings2, ShoppingBag, User, Video } from 'lucide-react';
-
-import {
-  AgentPanel,
-  ChatPanel,
-  ConfigPanel,
-  DancePanel,
-  LivePanel,
-  MarketPanel,
-  RolePanel,
-} from '@/panels';
-
-const apps = [
-  {
-    icon: User,
-    key: 'agent',
-    label: '联系人',
-    component: <AgentPanel />,
-  },
-  {
-    icon: Music2,
-    key: 'dance',
-    label: '跳舞',
-    component: <DancePanel />,
-  },
-  {
-    icon: MessageSquare,
-    key: 'chat',
-    label: '聊天',
-    component: <ChatPanel />,
-  },
-  {
-    icon: Video,
-    key: 'live',
-    label: '视频',
-    component: <LivePanel />,
-  },
-  {
-    icon: ShoppingBag,
-    key: 'market',
-    label: '商店',
-    component: <MarketPanel />,
-  },
-  {
-    icon: Settings2,
-    key: 'config',
-    label: '设置',
-    component: <ConfigPanel />,
-  },
-  {
-    icon: User,
-    key: 'role',
-    label: '角色',
-    component: <RolePanel />,
-  },
-];
 
 const Desktop = () => {
-  const [panel, setPanel] = useConfigStore((s) => [s.panel, s.setPanel]);
-
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', width: '100%', position: 'relative' }}>
       <Header />
-      <div>
-        <ActionIconGroup
-          items={apps}
-          onActionClick={(action) => {
-            setPanel(action.key as any, { open: true });
-          }}
-        />
-        {apps.map((app) => {
-          const open = panel[app.key as PanelKey].open;
-          const component = app.component;
-          return (
-            <div key={app.key} style={{ display: open ? 'flex' : 'none' }}>
-              {component}
-            </div>
-          );
-        })}
-      </div>
+      <Docker />
       <Background />
     </div>
   );
